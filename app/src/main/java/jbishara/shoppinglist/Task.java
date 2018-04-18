971c3b082c94e4f0b6ad821b02948246d5b5aa22
jbishara-tgm <jbishara@student.tgm.ac.at>
2018-04-18 13:19:48 +0200

    wenn dieses repo auch hinnig wird raste ich aus

---
+++ app/src/main/java/jbishara/shoppinglist/Task.java
@@ -0,0 +1,16 @@
+package jbishara.shoppinglist;
+
+/**
+ * Created by Johannes Bishara on 18.04.2018.
+ */
+
+public class Task {
+    private String task;
+    public Task() {}
+    public Task(String task) {
+        this.task = task;
+    }
+    public String getTask() {
+        return task;
+    }
+}
